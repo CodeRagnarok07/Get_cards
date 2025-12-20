972a7887e7318a58ac295cd3d03579cb17bb7dc8
angelr <angelr@noritex.com>
2025-12-19 22:22:23 -0400

    chore: Delete text-to-speech, dictionary, and Selenium-related test and utility files.

--- tests/text_to_speech.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-import os
-mytext= """Presentarte como una persona profesional"""
-language='es'
-myobj=gTTS(text=mytext,lang=language)
-myobj.save("test.mp3")
-
-os.system("mpg321 test.mp3")
